bb0ea43417f015634a5e0b51103dca80536a7441
Qiang Kou <qkou@qkou.info>
2020-02-24 21:33:31 -0800

    FP.1

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -24,30 +24,37 @@ private:
     
 };
 
-// FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject. 
-// The class shall have the public methods „void waitForGreen()“ and „void simulate()“ 
-// as well as „TrafficLightPhase getCurrentPhase()“, where TrafficLightPhase is an enum that 
-// can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“. 
-// Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value. 
+enum class TrafficLightPhase : char { red = 0, green = 1 };
 
-class TrafficLight
-{
-public:
-    // constructor / desctructor
+class TrafficLight : public TrafficObject {
+ public:
+  // constructor / desctructor
+  TrafficLight();
 
-    // getters / setters
+  // getters / setters
+  TrafficLightPhase getCurrentPhase();
 
-    // typical behaviour methods
+  // typical behaviour methods
+  void waitForGreen();
 
-private:
-    // typical behaviour methods
+  void simulate();
 
-    // FP.4b : create a private member of type MessageQueue for messages of type TrafficLightPhase 
-    // and use it within the infinite loop to push each new TrafficLightPhase into it by calling 
-    // send in conjunction with move semantics.
+ private:
+  // typical behaviour methods
+  TrafficLightPhase _currentPhase;
 
-    std::condition_variable _condition;
-    std::mutex _mutex;
+  void cycleThroughPhases();
+
+  // FP.4b : create a private member of type MessageQueue for messages of type
+  // TrafficLightPhase and use it within the infinite loop to push each new
+  // TrafficLightPhase into it by calling send in conjunction with move
+  // semantics.
+
+  std::shared_ptr<MessageQueue<TrafficLightPhase>> _queue =
+      std::make_shared<MessageQueue<TrafficLightPhase>>();
+
+  std::condition_variable _condition;
+  std::mutex _mutex;
 };
 
 #endif
